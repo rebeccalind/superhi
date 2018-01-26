cf6886b428fc5afea92d52377353add5efd534a7
Rebecca Lind <rebecca.lind@hyperisland.se>
2018-01-26 15:37:03 +0100

    Add files via upload

--- fifty/fifty.js
+++ fifty/fifty.js
@@ -13,17 +13,20 @@ function makeMarquee () {
 
 makeMarquee()
 
+function random(min, max) {
+    return Math.floor(Math.random () * (max - min + 1)) + min;
+}
 
 // animation circles
 
 const circles = document.querySelectorAll('.circle')
 
-circles.forEach(function(circle, index) {
+circles.forEach((circle, index) => {
   
     circle.animate([
-    { transform: 'scale(1)' },
-    { transform: 'scale(1.2)' },
-    { transform: 'scale(1)' }
+      { transform: 'scale(1)' },
+      { transform: 'scale(1.2)' },
+      { transform: 'scale(1)' }
 
   ], {
     delay: 300 * index,
@@ -32,6 +35,38 @@ circles.forEach(function(circle, index) {
   })
   
 })
+
+const squiggles = document.querySelectorAll('.squiggle')
+
+squiggles.forEach((squiggle, index) => {
+  
+  const randomNumber = random(0, 45)
+  
+  squiggle.animate([
+      { transform: 'rotate(0deg)' },
+      { transform: `rotate(${randomNumber}deg)`},
+      { transform: 'rotate(0deg)' }
+
+  ], {
+    delay: 300 * index,
+    duration: 5000,
+    iterations: Infinity
+  })
+  
+})
+
+// TRADITIONAL JS
+// var doSometing = function(foo) {}
+
+// MORE MODERN WAY OF JS
+// const doSomething = (foo) => {}
+
+// OLDER var REPLACED WITH const
+// var profile = name + ' is ' + age
+// const profile = `${name} is ${age}`
+// transform: 'rotate(' + randomNumber + 'deg)'
+// transform: `rotate(${randomNumber}deg)`,
+
 
 
 
