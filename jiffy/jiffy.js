71da36efde9d3f3f829ddb1159e478f1772957c1
Rebecca Lind <rebecca.lind@hyperisland.se>
2018-02-13 22:21:23 +0100

    Add files via upload

--- jiffy/jiffy.js
+++ jiffy/jiffy.js
@@ -1,23 +1,80 @@
-fetch('https://api.giphy.com/v1/gifs/search?api_key=fdICJYJLE5msINhHiV5TlyNPB5gcagI2&q=dog&limit=50&offset=0&rating=PG-13&lang=en')
+const API_KEY = 'fdICJYJLE5msINhHiV5TlyNPB5gcagI2'
 
-  .then(response => {
-  return response.json();
-})
+const randomChoice = arr => {
+  const randIndex = Math.floor(Math.random() * arr.length)
+  return arr[randIndex]
+}
 
-  .then(json => {
-  const gif = json.data[0]
-  const src = gif.images.original.mp4
-  
-  const video = document.createElement('video')
+function createVideo (src) {
+	const video = document.createElement('video')
   
   video.src = src
   video.autoplay = true
   video.loop = true
-  
-  const videosEl = document.querySelector('.videos')
-  videosEl.appendChild(video)
-})
+  video.className = 'video'
 
-.catch(error => {
+  return video
+}
+
+const toggleLoading = state => {
+  if (state) {
+    document.body.classList.add('loading')
+  } else {
+    document.body.classList.remove('loading')
+  }
+}
   
-})
+const searchGiphy = searchTerm => {
+  
+  toggleLoading(true)
+    
+  fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchTerm}&limit=50&offset=0&rating=PG-13&lang=en`)
+
+    .then(response => {
+    return response.json();
+  })
+
+    .then(json => {
+    const gif = randomChoice(json.data)
+    const src = gif.images.original.mp4
+
+    const video = createVideo(src)
+
+    const videosEl = document.querySelector('.videos')
+  //   videosEl.appendChild(video)
+    toggleLoading(false)
+  })
+
+  .catch(error => {
+  })
+}
+
+const searchEl = document.querySelector('.search-input')
+
+const hintEl = document.querySelector('.search-hint')
+
+const doSearch = event => {
+  const searchTerm = searchEl.value
+  
+  if (searchTerm.length > 2) {
+    hintEl.innterHTML = `Hit enter to search ${searchTerm}`
+    document.body.classList.add('.show-hint')
+  } else {
+    document.body.classList.remove('.show-hint')
+  }
+  
+  if (event.key === 'Enter' && searchEl.value.length > 2) {
+    searchGiphy(searchTerm)
+  }
+}
+
+searchEl.addEventListener('keyup', doSearch)
+
+
+
+
+
+
+
+
+
